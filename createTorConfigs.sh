dc21c40c99a4e28ce339757b2832bc9def89df14
pablomendezroyo <mendez4a@gmail.com>
2023-06-28 17:26:08 +0200

    update ethical metrics endpoint

--- createTorConfigs.sh
+++ createTorConfigs.sh
@@ -10,5 +10,5 @@ for i in $(seq 1 $NUMBER_OF_TOR_INSTANCES); do
   echo "SOCKSPort 127.0.0.1:$((9050 + i))" > "/etc/tor/torrc.$i"
   echo "DataDirectory /var/lib/tor$i" >> "/etc/tor/torrc.$i"
   echo "HiddenServiceDir /var/lib/tor/hidden_service$i/" >> "/etc/tor/torrc.$i"
-  echo "HiddenServicePort 9090 ethical-metrics.ethical-metrics.dappnode:9090" >> "/etc/tor/torrc.$i"
+  echo "HiddenServicePort 9090 ethical-metrics.ethical-metrics-multiple.dappnode:9090" >> "/etc/tor/torrc.$i"
 done
